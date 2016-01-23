ab1b53c2a52ca9649d2c37cfe78b08271ef0a79d
imotai <codego.me@gmail.com>
2016-01-23 21:31:29 +0800

    add set_vim.sh

---
+++ set_vim.sh
@@ -0,0 +1,5 @@
+mv ~/.vim ~/.vim.orig
+mv ~/.vimrc ~/.vimrc.orig
+git clone git://github.com/fuhu/.vimrc.git ~/.vim
+ln -s ~/.vim/vimrc ~/.vimrc
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
