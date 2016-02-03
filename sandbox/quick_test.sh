9c6fc2ea60aa8d9c87df325f2a0aacbbcfa6b919
wangtaize <codego.me@gmail.com>
2016-02-03 14:53:58 +0800

    run container on engine

--- sandbox/quick_test.sh
+++ sandbox/quick_test.sh
@@ -1,22 +1,8 @@
 #!/usr/bin/env sh
 SANDBOX_DIR=`pwd`
-killall dos_ce
-if [ -d "dfs" ]
-then
-  cd dfs
-  umount rootfs/proc
-  umount rootfs/sys
-  umount rootfs/dev/pts
-  umount rootfs/dev/shm
-  umount rootfs/dev/mqueue
-  umount rootfs/dev
-  rm -rf rootfs/dev/*
-  cd -
-fi
-
-mkdir -p dfs/rootfs
-tar -zxvf rootfs.tar.gz -C dfs/rootfs
-cp runtime.json dfs/
-cp config.json dfs/
-#cd dfs && nohup ../../dos_ce daemon --ce_bin_path=/vagrant/dos/dos_ce >log 2>&1 & 
+#cd dfs && nohup ../../dos_ce daemon --ce_bin_path=/vagrant/dos/dos_ce >log 2>&1 &
+mkdir -p work_dir
+mkdir -p gc_dir
+echo "--ce_work_dir=$SANDBOX_DIR/work_dir" > dos.flags
+ 
 
--- sandbox/start_all.sh
+++ sandbox/start_all.sh
@@ -1,9 +1,15 @@
-rm -rf core
+rm -rf core dos
+cp ../dos dos
 sh stop_all.sh
+sh clean_work_dir.sh
+echo "start engine"
+nohup ./dos engine --ce_bin_path=`pwd`/dos --ce_work_dir=`pwd`/work_dir --ce_gc_dir=`pwd`/gc_dir > engine.log 2>&1 &
+sleep 3
 nohup ../dos master >master.log 2>&1 &
 sleep 3
 nohup ../dos let >let.log 2>&1 &
 nohup ../dos scheduler >scheduler.log 2>&1 &
+
 #sleep 5
 #./dos_ce --ce_endpoint=127.0.0.1:7676 run -u http://idcos.io/dfs.tar.gz -n dfs
 
