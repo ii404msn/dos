2bda7258bfa87f96901d90aef52e3ef53fbd5fc8
imotai <codego.me@gmail.com>
2016-02-17 23:53:06 +0800

    fix compile err

--- kernel/src/master/master_impl.cc
+++ kernel/src/master/master_impl.cc
@@ -32,7 +32,7 @@ void MasterImpl::GetScaleUpPod(RpcController* controller,
                                GetScaleUpPodResponse* response,
                                Closure* done) {
   // get pods that need to be scheduled
-  pod_manager_->GetScaleUpPods(request.condition(),
+  pod_manager_->GetScaleUpPods(request->condition(),
                                response->mutable_pods());
   response->set_status(kRpcOk);
   done->Run();
