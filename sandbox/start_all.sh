0229ea30ce8b0ed5dbb493095a1a42bc74c036e9
wangtaize <codego.me@gmail.com>
2016-02-24 19:38:39 +0800

    mv subcommand

--- sandbox/start_all.sh
+++ sandbox/start_all.sh
@@ -1,11 +1,16 @@
 rm -rf core dos
 cp ../dos dos
+ln -s -f ./dos master
+ln -s -f ./dos scheduler
+ln -s -f ./dos let
+ln -s -f ./dos engine
+ln -s -f ./dos initd
 sh stop_all.sh
 sh clean_work_dir.sh
 echo "start engine"
-nohup ./dos engine --ce_process_default_user=root --ce_bin_path=`pwd`/dos --ce_work_dir=`pwd`/work_dir --ce_gc_dir=`pwd`/gc_dir > engine.log 2>&1 &
+nohup ./engine --ce_process_default_user=root --ce_bin_path=`pwd`/initd --ce_work_dir=`pwd`/work_dir --ce_gc_dir=`pwd`/gc_dir > engine.log 2>&1 &
 sleep 3
-nohup ../dos master >master.log 2>&1 &
+nohup ./master >master.log 2>&1 &
 sleep 3
-nohup ../dos let --agent_port_range_end=7000 --agent_port_range_start=4000>let.log 2>&1 &
-nohup ../dos scheduler >scheduler.log 2>&1 &
+nohup ./let --agent_port_range_end=7000 --agent_port_range_start=4000>let.log 2>&1 &
+nohup ./scheduler >scheduler.log 2>&1 &
