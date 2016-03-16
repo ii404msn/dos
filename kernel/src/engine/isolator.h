4f02b6b267fa00e02505d62927e8d3db2e07e925
wangtaize <codego.me@gmail.com>
2016-03-16 23:17:41 +0800

    add some comment

--- kernel/src/engine/isolator.h
+++ kernel/src/engine/isolator.h
@@ -12,10 +12,18 @@ public:
   CpuIsolator(const std::string& path);
   ~CpuIsolator();
   // attach pid this isolator
+  // like echo pid >> path/cgroup.proc
   bool Attach(int32_t pid);
+  // assign quota to cpu subsystem
+  // by update the cpu.share file
   bool AssignQuota(int32_t quota);
+  // set the max cpu peroid that used
+  // by update the cpu.cfs_peroid_quota
   bool AssignLimit(int32_t limit);
+  // if path does not exist then create it
   bool Init();
+  // Get the cpu used in a peroid
+  bool GetCpuUsage(int32_t* used);
 private:
   std::string path_;
   int32_t quota_;
