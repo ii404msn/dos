f35ca231aa40ddac1846e709f7136a76536463b2
wangtaize <codego.me@gmail.com>
2016-05-15 11:07:01 +0800

    add log engine

---
+++ kernel/src/engine/log_engine.h
@@ -0,0 +1,15 @@
+#ifndef KERNEL_ENGINE_LOG_ENGINE_H
+#define KERNEL_ENGINE_LOG_ENGINE_H
+
+namespace dos {
+
+// log engine traces stat and important events for container
+// the first version is implemented by leveldb which stores data on
+// local disk
+class LogEngine {
+
+};
+
+}
+
+#endif
