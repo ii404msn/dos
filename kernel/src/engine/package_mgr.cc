a7ba5f10915d6aa03546422f586012d1d74a527e
wangtaize <codego.me@gmail.com>
2016-05-17 22:36:11 +0800

    update package manager

--- kernel/src/engine/package_mgr.cc
+++ kernel/src/engine/package_mgr.cc
@@ -13,7 +13,12 @@ DECLARE_string(ce_package_mgr_initd_port);
 namespace dos {
 
 PackageMgr::PackageMgr(leveldb::DB* db):db_(db),
-  mutex_(){}
+  mutex_(), proc_mgr_(NULL), 
+  endpoint_(), initd_stub_(NULL),
+  rpc_client_(NULL){
+  proc_mgr_ = new ProcessMgr();
+  rpc_client_ = new RpcClient();
+}
 
 PackageMgr::~PackageMgr() {}
 
@@ -29,7 +34,29 @@ bool PackageMgr::Init() {
   if (!get_ok.ok()) {
     endpoint = "127.0.0.1:" + FLAGS_ce_package_mgr_initd_port;
   }
-  LOG(INFO, "package mgr initd endpoint %s", endpoint.c_str()):
+  endpoint_ = endpoint;
+  LOG(INFO, "package mgr initd endpoint %s", endpoint.c_str());
+  return true;
+}
+
+bool PackageMgr::CheckInitd() {
+  MutexLock lock(&mutex_);
+  if (!initd_stub_) {
+    rpc_client_->GetStub(endpoint_, &initd_stub_);
+  }
+  StatusRequest request;
+  StatusResponse response;
+  bool ok = rpc_client_->SendRequest(initd_stub_, &Initd_Stub::Status,
+                                    &request, &response, 5, 1);
+  if (!ok) {
+    LOG(WARNING, "initd with endpoint %s is dead", endpoint_.c_str());
+    return false;
+  }
+  return true;
+}
+
+bool PackageMgr::LaunchInitd() {
+  MutexLock lock(&mutex_);
 }
 
 }
